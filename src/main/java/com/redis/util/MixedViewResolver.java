d8a178c603e8f3e7835b64fd91b668cd6577b1e6
RoyChan <690374857@qq.com>
2016-10-12 19:56:32 +0800

    对spring进行mvc配置

---
+++ src/main/java/com/redis/util/MixedViewResolver.java
@@ -0,0 +1,36 @@
+package com.redis.util;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2015/11/26.
+ */
+public class MixedViewResolver implements ViewResolver {
+    private Map<String,ViewResolver> resolvers;
+
+    public void setResolvers(Map<String,ViewResolver> resolvers){
+        this.resolvers = resolvers;
+    }
+
+
+    @Override
+    public View resolveViewName(String viewName, Locale locale) throws Exception {
+        int n = viewName.lastIndexOf(".");
+        if(n != -1){
+            String suffix = viewName.substring(n+1);
+            ViewResolver resolver = resolvers.get(suffix);
+            if(resolver == null){
+                throw new RuntimeException("No ViewResolver For" + suffix);
+            }
+            return resolver.resolveViewName(viewName, locale);
+        }else {
+            ViewResolver resolver = resolvers.get("html");
+            return resolver.resolveViewName(viewName, locale);
+        }
+
+    }
+}
