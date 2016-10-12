8831bee053020c804fd4baddf11f3a80bf6f369d
RoyChan <690374857@qq.com>
2016-10-12 19:57:35 +0800

    增加一个命令查询的页面

--- src/main/java/com/redis/web/CommondController.java
+++ src/main/java/com/redis/web/CommondController.java
@@ -1,7 +1,24 @@
 package com.redis.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2016/10/11.
  */
+@Controller
+@RequestMapping(value = "/command")
 public class CommondController {
+
+    @RequestMapping()
+    public String index() {
+        // 返回知识点管理列表页面question_knowledge_list
+        System.out.println("11111");
+        return "command_list";
+    }
+
+    @RequestMapping(value = "/list")
+    public void list(){
+        System.out.println("hello world");
+    }
 }
