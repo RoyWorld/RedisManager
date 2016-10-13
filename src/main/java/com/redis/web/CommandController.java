5ad461ba0e5c5335c498f0f0f9f80fe415b20657
RoyChan <690374857@qq.com>
2016-10-13 16:40:51 +0800

    增加一个测试返回json的测试接口

--- src/main/java/com/redis/web/CommandController.java
+++ src/main/java/com/redis/web/CommandController.java
@@ -1,7 +1,10 @@
 package com.redis.web;
 
+import com.redis.crawler.Command;
+import com.redis.util.HashMapOperator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2016/10/11.
@@ -15,4 +18,11 @@ public class CommandController {
         return "command_list";
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/command_list")
+    public Object commandObject(){
+        HashMapOperator<String, Command> hashMapOperator = HashMapOperator.getHashMapOperator();
+        Command command = hashMapOperator.hGet("hash:HSET", Command.class);
+        return command;
+    }
 }
