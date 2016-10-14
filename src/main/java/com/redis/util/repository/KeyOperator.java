9346b98ea38c30bf4155ef0eb8ab50efdc257acb
RoyChan <690374857@qq.com>
2016-10-14 09:19:44 +0800

    增加一个对key操作的工具类

---
+++ src/main/java/com/redis/util/repository/KeyOperator.java
@@ -0,0 +1,105 @@
+package com.redis.util.repository;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.connection.convert.Converters;
+import org.springframework.data.redis.connection.jedis.JedisConverters;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationUtils;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import redis.clients.jedis.Jedis;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class KeyOperator<K, V> {
+
+    private static final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+
+    private static final RedisTemplate redisTemplate = (RedisTemplate) context.getBean("redisTemplate");
+
+    private volatile static KeyOperator keyOperator;
+
+    public static KeyOperator getKeyOperator(){
+        if (keyOperator == null){
+            synchronized (HashMapOperator.class){
+                if (keyOperator == null){
+                    keyOperator = new KeyOperator();
+                }
+            }
+        }
+        return keyOperator;
+    }
+
+    private  BoundValueOperations<K, V> getBoundValueOps(final K key){
+        return redisTemplate.boundValueOps(key);
+    }
+
+    // Key
+
+    public void del(final K key) {
+        redisTemplate.delete(key);
+    }
+
+    public void del(final Collection<K> keys) {
+        redisTemplate.delete(keys);
+    }
+
+    public Boolean exists(final K key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    public Boolean expire(final K key,final long timeout,final TimeUnit unit) {
+        return redisTemplate.expire(key, timeout, unit);
+    }
+
+    public void expireAt(final K key,Date date) {
+        redisTemplate.expireAt(key, date);
+    }
+
+    public Set<K> keys(final K pattern) {
+        return redisTemplate.keys(pattern);
+    }
+
+    public String type(final K key) {
+        return redisTemplate.type(key).code();
+    }
+
+    public V get(final K key) {
+        BoundValueOperations<K,V> ops  = this.getBoundValueOps(key);
+        return ops.get();
+    }
+
+    public V getSet(final K key,final V value) {
+        BoundValueOperations<K,V> ops  = this.getBoundValueOps(key);
+        return ops.getAndSet(value);
+    }
+
+    public Long incr(final K key,final long delta) {
+        BoundValueOperations<K,V> ops  = this.getBoundValueOps(key);
+        return ops.increment(delta);
+    }
+
+    public void set(final K key,final V value) {
+        BoundValueOperations<K,V> ops  = this.getBoundValueOps(key);
+        ops.set(value);
+    }
+
+    public void set(final K key,final V value,final long timeout,final TimeUnit unit) {
+        BoundValueOperations<K,V> ops  = this.getBoundValueOps(key);
+        ops.set(value, timeout, unit);
+    }
+
+    public void rename(final K oldkey, final K newKey){
+        redisTemplate.rename(oldkey, newKey);
+    }
+
+}
