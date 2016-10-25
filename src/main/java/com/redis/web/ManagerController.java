86148aa23b0bde2cb40fdb3ca8b4323349d19d33
陈荣源 <690374857@qq.com>
2016-10-25 18:18:29 +0800

    新增一个keysearch的接口，并对manager页面进行调整

--- src/main/java/com/redis/web/ManagerController.java
+++ src/main/java/com/redis/web/ManagerController.java
@@ -1,16 +1,48 @@
 package com.redis.web;
 
+import com.redis.util.repository.ConfigOperator;
+import com.redis.util.repository.KeyOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+
+import java.util.*;
 
 /**
  * Created by Administrator on 2016/10/17.
  */
 @Controller
-@RequestMapping(value = "/redis")
+@RequestMapping(value = "/redis/manager")
 public class ManagerController {
-    @RequestMapping(value = "/manager")
+
+    @Autowired
+    ConfigOperator configOperator;
+
+    @Autowired
+    KeyOperator keyOperator;
+
+
+    @RequestMapping(value = "")
     public String index(){
         return "manager";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/database")
+    public List database(){
+        List l = configOperator.getDatabaseNum();
+        List<Map<String, Object>> databaseList = new ArrayList<>();
+        for (int i = 0; i < Integer.parseInt((String) l.get(1)); i++) {
+            boolean result = configOperator.selectDatabase(i);
+            Set<String> keySet = keyOperator.keys("*");
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "db" + i);
+            map.put("keys", keySet);
+            map.put("display", false);
+            databaseList.add(map);
+        }
+        return databaseList;
     }
 }
--- src/main/webapp/js/redis/manager.js
+++ src/main/webapp/js/redis/manager.js
@@ -10,30 +10,60 @@ modelApp.controller('modelListCtrl', function($scope, $rootScope, $http){
 
     function init(){
         $scope.connections = ["connectio", "fiang", "dfina3"];
+        $scope.query();
     }
 
-    //查command的list
+    //查database中所有keys
     $scope.query = function (){
-        var params = {};
-        if ($scope.commandGroup != null && $scope.commandGroup != ''){
-            params["group"] = $scope.commandGroup;
-        }
-        if ($scope.commandName != null && $scope.commandName != ''){
-            params["name"] = $scope.commandName;
-        }
         $http({
             method: "get",
-            url: "/redis/command_list",
-            params:params,
+            url: "/redis/manager/database",
             headers: {
                 contentType: 'application/json;charset=UTF-8'
             }
         }).success(function (data) {
             if (data.rtnCode == "0000000") {
-                $scope.commandList = data.bizData;
+                $scope.databaseList = data.bizData;
             }
         });
     }
+
+    //侧边树状信息栏的点击事件
+    $scope.treeToggle = function(databaseTree) {
+        for (var database in $scope.databaseList){
+            if (databaseTree.name == $scope.databaseList[database].name){
+                $scope.databaseList[database].display = !$scope.databaseList[database].display;
+                break;
+            }
+        }
+    }
+
+
+    $('.tree-toggle').click(function() {
+        $(this).parent().children('ul.tree').toggle(200);
+    });
+    
+    
+    $(".nav-tabs").on("click", "a", function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('add-contact')) {
+            $(this).tab('show');
+        }
+    }).on("click", "span", function () {
+        var anchor = $(this).siblings('a');
+        $(anchor.attr('href')).remove();
+        $(this).parent().remove();
+        $(".nav-tabs li").children('a').first().click();
+    });
+
+    $('.add-contact').click(function (e) {
+        e.preventDefault();
+        var id = $(".nav-tabs").children().length; //think about it ;)
+        var tabId = 'contact_' + id;
+        $(this).closest('li').before('<li><a href="#contact_' + id + '">New Tab</a></li>');
+        $('.tab-content').append('<div class="tab-pane" id="' + tabId + '">Contact Form: New Contact ' + id + '</div>');
+        $('.nav-tabs li:nth-child(' + id + ') a').click();
+    });
 
     init();
 
